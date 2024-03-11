3fa08fd474935f5172ea77d7f95046f967dd5bcc
flashl1ght <flashlight.studio@pm.me>
2024-03-11 02:37:26 +0100

    added session manager from scs package

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -3,26 +3,23 @@ package main
 import (
 	"net/http"
 
-	"github.com/flashl1ght/myFirstGoApp/pkg/config"
 	"github.com/gorilla/csrf"
 )
 
-var app *config.AppConfig
-
-// NewMiddleware sets the config for middleware
-func NewMiddleware(a *config.AppConfig) {
-	app = a
-}
-
-// CSRFMiddleware creates middleware responsible for CSRF tokens
+// CSRFMiddleware adds CSRF protection
 func CSRFMiddleware(next http.Handler) http.Handler {
 	csrfHandler := csrf.Protect(
 		app.CSRFAuthKey,
 		csrf.HttpOnly(true),
 		csrf.Path("/"),
 		csrf.SameSite(csrf.SameSiteLaxMode),
-		csrf.Secure(false), // remove before going to production
+		csrf.Secure(app.InProduction),
 	)
 
 	return csrfHandler(next)
+}
+
+// SessionLoad loads and saves the session on every request
+func SessionLoad(next http.Handler) http.Handler {
+	return sessionManager.LoadAndSave(next)
 }
